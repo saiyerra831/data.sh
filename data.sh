0de5e03bc2c4462dbc0abfab0ddcf9e1d0850ea9
saiyerra831 <130850844+saiyerra831@users.noreply.github.com>
2023-07-13 00:17:03 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+sudo yum install git -y
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+sudo git clone https://saiyerra831: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@github.com/saiyerra831/tf-abalone.git
+cd tf-abalone
+sudo terraform validate
+sudo terraform plan
+sudo terraform apply --auto-approve
