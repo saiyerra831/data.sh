c4a63ad810d608c6473d0dd77ba5603dea9fd476
saiyerra831 <130850844+saiyerra831@users.noreply.github.com>
2023-07-13 10:54:59 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,7 +4,6 @@ sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 sudo git clone https://saiyerra831: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@github.com/saiyerra831/tf-abalone.git
-cd /
 cd tf-abalone
 sudo terraform validate
 sudo terraform plan
